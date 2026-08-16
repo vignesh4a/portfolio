3fcbe3665671f9585faef5d5158f404a5ed80fb8
vicky <vignesh4v@gmail.com>
2026-08-16 15:03:04 +0530

    Trigger clean build for dist deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace 'YOUR_REPOSITORY_NAME' with your exact GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', 
+  base: '/portfolio/', // Exact GitHub repository name
 })
